361b830d5d2cccd5d3c4d53ecd0fb943f1b4765d
thomasjnliu <thomasjnliu@gmail.com>
2022-01-15 15:44:21 -0500

    Add api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const fetchImages = () => {
+    fetch(
+      `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}&count=10`
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
+  // fetch images from api
+  useEffect(() => {
+    fetchImages();
+  }, []);
+  return <div className="App"></div>;
 }
 
 export default App;
